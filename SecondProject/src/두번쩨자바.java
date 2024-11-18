7e3578fddbaefc177ed443b7277076d70ed9c686
SIST-114 <SIST-114@DESKTOP-PMLC3NV>
2024-11-18 14:57:43 +0900

    121111

--- SecondProject/src/두번쩨자바.java
+++ SecondProject/src/두번쩨자바.java
@@ -3,7 +3,8 @@ public class 두번쩨자바 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("두번째 자바");
+		System.out.println("두번째 자바111");
+		
 
 	}
 
