fa2380885a34bb1171be1cc76f343278f3433e78
SIST-114 <SIST-114@DESKTOP-PMLC3NV>
2024-11-18 14:28:50 +0900

    111

---
+++ FirstProject/src/첫번쩨자바.java
@@ -0,0 +1,12 @@
+/*
+
+*/
+public class 첫번쩨자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println(hyuk joon);
+	}
+
+}
