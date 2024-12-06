adb764b5e08b90142ebf1e06627e6259239dddfb
SIST-114 <SIST-114@DESKTOP-PMLC3NV>
2024-12-06 09:32:09 +0900

    1111

--- Chapter03_제어문(조건문)/src/자바제어문_단일조건문2.java
+++ Chapter03_제어문(조건문)/src/자바제어문_단일조건문2.java
@@ -1,7 +1,7 @@
-// => 정수 2개 => 사용자 요청 => 계산
+// => 정수 2개 => 사용자 요청 => 계산 
 //              + , - , * /
 // 간단한 계산기 (사칙연산 사용)
-import java.util.Scanner;
+import java.util.Scanner; 
 
 import javax.swing.JOptionPane;
 public class 자바제어문_단일조건문2 {
