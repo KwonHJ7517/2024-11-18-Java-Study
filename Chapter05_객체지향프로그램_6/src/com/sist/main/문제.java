d70205f1d17cf0edc236114bce876b4fed8fbfb9
SIST-114 <SIST-114@DESKTOP-PMLC3NV>
2024-12-17 10:42:49 +0900

    1111

---
+++ Chapter05_객체지향프로그램_6/src/com/sist/main/문제.java
@@ -0,0 +1,23 @@
+package com.sist.main;
+class Member{
+	String name;
+	String id;
+	String pwd;
+	int age;
+	
+	Member(String name, String id) {
+		
+		this.name = name;
+		this.id = id;
+	}
+
+}
+public class 문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Member("hong","admin");
+		
+	}
+
+}
