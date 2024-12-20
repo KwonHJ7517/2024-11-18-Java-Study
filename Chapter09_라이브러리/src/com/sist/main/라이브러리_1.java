4b38fecdec4152df7727c7f6da65a543db203cb0
SIST-114 <SIST-114@DESKTOP-PMLC3NV>
2024-12-20 09:29:19 +0900

    1111

---
+++ Chapter09_라이브러리/src/com/sist/main/라이브러리_1.java
@@ -0,0 +1,34 @@
+package com.sist.main;
+/*
+ *    자바에서 지원하는 라이브러리
+ *                 ------ 프로그램에 필요한 클래스를 모아서 개발시 사용가능
+ *    => 라이브러리 중심 => 조립
+ *    => Scanner / System / String ...
+ *    
+ *    => 라이브러리
+ *       1) 자바에서 지원
+ *          java.lang
+ *          java.util
+ *          java.io
+ *          java.sql
+ *          java.net
+ *          java.text
+ *          java.langreflect    
+ *          ------------------ 1.0 버전
+ *          => javax.http
+ *          => javax.servlet.http
+ *          ---------------------- 2.0 버전
+ *          => org.springframework
+ *       2) 외부업체 지원 : Jsoup / lombok / Spring
+ *          => mvnrepository.com
+ *          => simple
+ *       3) 사용자 정의
+ */
+public class 라이브러리_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
